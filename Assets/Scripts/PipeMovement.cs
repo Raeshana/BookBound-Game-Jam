ff2908df4224c83efc83f2623a6ad397a07eb4cd
tu2463 <117417955+tu2463@users.noreply.github.com>
2023-12-29 23:06:31 +0800

    everything environment prototype

--- Assets/Scripts/PipeMovement.cs
+++ Assets/Scripts/PipeMovement.cs
@@ -5,26 +5,50 @@ using UnityEngine;
 public class PipeMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float speed = 5f;
+    public float speed;
+    public int direction;
+    public string type;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        StartCoroutine(Countdown());
+        if (type == "vertical")
+        {
+            StartCoroutine(Vertical());
+        }
+        else
+        {
+            StartCoroutine(Horizontal());
+        }
     }
 
-    // Update is called once per frame
-    private IEnumerator Countdown()
+    private IEnumerator Vertical()
     {
         Debug.Log(rb.velocity.y);
         bool running = true;
         while (running)
         {
-            yield return new WaitForSeconds(2f);
-            rb.velocity = new Vector2(rb.velocity.x, speed);
-            yield return new WaitForSeconds(2f);
-            rb.velocity = new Vector2(rb.velocity.x, speed * -1);
-            yield return new WaitForSeconds(2f);
+            rb.velocity = new Vector2(rb.velocity.x, speed * direction);
+            yield return new WaitForSeconds(0.5f);
+            rb.velocity = new Vector2(rb.velocity.x, speed * direction * -1);
+            yield return new WaitForSeconds(0.5f);
+            rb.velocity = Vector2.zero;
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+    private IEnumerator Horizontal()
+    {
+        Debug.Log(rb.velocity.x);
+        bool running = true;
+        while (running)
+        {
+            rb.velocity = new Vector2(speed * direction, rb.velocity.y);
+            yield return new WaitForSeconds(0.5f);
+            rb.velocity = new Vector2(speed * direction * -1, rb.velocity.y);
+            yield return new WaitForSeconds(0.5f);
+            rb.velocity = Vector2.zero;
+            yield return new WaitForSeconds(1.5f);
         }
     }
 }
