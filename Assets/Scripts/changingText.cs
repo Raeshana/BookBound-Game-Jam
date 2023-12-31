c44dd6ff73bdd38fc8efbded86d7fc57ee2712ae
tu2463 <117417955+tu2463@users.noreply.github.com>
2024-01-01 01:17:36 +0800

    Deleted extra semicolon in changingText.cs

--- Assets/Scripts/changingText.cs
+++ Assets/Scripts/changingText.cs
@@ -7,7 +7,7 @@ public class changingText : MonoBehaviour
 {
     private TMP_Text text;
     [SerializeField] float time;
-    private string[] words = {"Adieu", "Aufwiedersehen", "Gesundheit", "Farewell", "Goodbye"};;
+    private string[] words = {"Adieu", "Aufwiedersehen", "Gesundheit", "Farewell", "Goodbye"};
     private int counter = 0;
 
     // Start is called before the first frame update
